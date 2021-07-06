52493428c599b6d8c1563d87aaf823ccc2910f74
Zach Green <zachary.green@projectn95.com>
2021-07-06 18:09:14 -0400

    favorites/favoriting functionality and some minor styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,9 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles ((theme) => ({
-    root: {
-        margin: theme.spacing(1),
-        width: '25ch',
-    }
-}))
 
 export default function Home (props) {
-    const classes = useStyles();
     const [userName, setUserName] = useState('');
 
     const updateUserName = (event) => {
@@ -25,7 +17,7 @@ export default function Home (props) {
 
     return (
         <div className='home-content-div'>
-            <form className={classes.root} noValidate autoComplete='off'>
+            <form className='form' noValidate autoComplete='off'>
                 <TextField id='outlined-basic' label='username' onChange={updateUserName}/>
                 <Button
                     className='search-gists-button'
